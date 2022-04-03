989feaf52b3d241b49cd55a5f62089c80eba6f21
nicolopedrani <nicolo.pedrani@gmail.com>
2022-04-03 13:14:19 +0200

    update graph

--- Toy_Model/utils.py
+++ Toy_Model/utils.py
@@ -142,3 +142,95 @@ def fit_timeacorr(descriptors_names,data,axs=None):
 
     plt.show()
 
+#-- plot model loss function --#
+def plot_model_lossfunction(model):
+
+    n_eig=len(model.nn[-1].bias)
+
+    fig, axs = plt.subplots(1,2,figsize=(12,5))#,dpi=100)
+
+    loss_train = [x.cpu() for x in model.loss_train]
+    loss_valid = [x.cpu() for x in model.loss_valid]
+
+    # Loss function
+    ax = axs[0]
+    ax.plot(loss_train,'-',label='Train')
+    ax.plot(loss_valid,'--',label='Valid')
+    ax.set_ylabel('Loss Function')
+
+    # Eigenvalues vs epoch
+    ax = axs[1]
+    with torch.no_grad():
+        evals_train = np.asarray(torch.cat(model.evals_train).cpu())
+    for i in range(n_eig):
+        ax.plot(evals_train[:,i],label='Eig. '+str(i+1))
+    ax.set_ylabel('Eigenvalues')
+    
+    # Common setup
+    for ax in axs:
+        if model.earlystopping_.best_epoch is not None:
+            if model.earlystopping_.early_stop:
+                ax.axvline(model.earlystopping_.best_epoch,ls='dotted',color='grey',alpha=0.5,label='Early Stopping')
+                ax.set_xlabel('#Epochs')
+                ax.legend(ncol=2)
+
+    plt.tight_layout()
+
+def plot_cvs_isolines(model,limits,points=150,n_out=2,scatter=None,axs=None):
+
+    #-- prepare grid --#
+    xx,yy = np.meshgrid(np.linspace(limits[0][0],limits[0][1],points),np.linspace(limits[1][0],limits[1][1],points))
+    grid = np.transpose(np.array([xx.reshape(points*points),yy.reshape(points*points)]))
+
+    #-- evaluate cvs on the grid --#
+    cvs = []
+    for i in range(n_out):
+        cvs.append(np.transpose(model(torch.Tensor(grid)).detach().cpu().numpy())[i].reshape(points,points))
+
+    #-- plotting --#
+    if axs is None:
+        fig,axs = plt.subplots(1,n_out,figsize=(12,6))
+
+    for k,ax in enumerate(axs):
+        cset = ax.contourf(xx,yy,cvs[k],linewidths=1,cmap="fessa")
+        cset = ax.contour(xx,yy,cvs[k],linewidths=3,cmap="gray",linestyles="dashed")
+        ax.clabel(cset,inline=True,fmt='%1.1f',fontsize=15)
+        ax.set_xlabel("p.x")
+        ax.set_ylabel("p.y")
+        ax.set_title('Deep-TICA '+str(k+1))
+        if scatter is not None:
+            ax.scatter(scatter[:,0],scatter[:,1],s=2,c='white',alpha=0.8)
+            ax.set_aspect('equal')
+        
+    plt.tight_layout()
+    plt.show()
+
+def Boltzmann_product(model0,model1,X,j=0,k=1,logweight=None,normed=False):
+
+    if logweight is not None:
+            weights= np.exp(logweight)
+    else:
+        weights=np.ones(X.shape[0])
+
+    a = np.transpose(model0(torch.Tensor(X)).detach().cpu().numpy())[j]
+    b = np.transpose(model1(torch.Tensor(X)).detach().cpu().numpy())[k]
+    sumtot = np.multiply(np.multiply(a,weights),b).mean()
+
+    if normed:
+        sumcv1 = np.multiply(np.multiply(a,weights),a).mean()
+        sumcv2 = np.multiply(np.multiply(b,weights),b).mean()
+        return sumtot/np.sqrt(sumcv1*sumcv2)
+    else:
+        return sumtot
+
+def orthogonal_cv(model,X,logweight=None,j=0,k=1):
+
+    a = np.transpose(model(torch.Tensor(X)).detach().cpu().numpy())[j]
+    b = np.transpose(model(torch.Tensor(X)).detach().cpu().numpy())[k]
+
+    prod_ab = Boltzmann_product(model,model,X,logweight=logweight,j=j,k=k)
+    prod_aa = Boltzmann_product(model,model,X,logweight=logweight,j=j,k=j) 
+
+    c = b - prod_ab/(prod_aa) * a
+
+    return c
