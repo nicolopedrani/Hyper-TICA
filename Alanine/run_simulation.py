9c9d7c0a74f729eca65c90197fe68fc36e03c7ac
nicolopedrani <nicolo.pedrani@gmail.com>
2022-05-01 11:20:03 +0200

    finish simulations

--- Alanine/run_simulation.py
+++ Alanine/run_simulation.py
@@ -1,5 +1,3 @@
-#seeds : 4045 35 151 500
-
 #-- to run process from jupyter --#
 import subprocess
 import os
@@ -29,15 +27,7 @@ sim_parameters = {
     'plot_max_fes' :70,
 }
 
-import sys
-if len(sys.argv)!=3:
-    print("please insert the lag time and the seed")
-    sys.exit("error")
-
-#lag -> sys.argv[1]
-#seed -> sys.argv[2]
-
-folder = "test_different_seeds/lag"+sys.argv[1]+"/bias1_seed"+sys.argv[2]+"/"
+folder = "unbias_A/bias1/bias2_02/"
 
 Path(folder).mkdir(parents=True, exist_ok=True)
 
@@ -53,7 +43,7 @@ ene: ENERGY
 # include descriptors
 INCLUDE FILE=plumed_descriptors.data
 # define cv
-deep: PYTORCH_MODEL FILE=../deeptica_seed"""+sys.argv[2]+"""/model.ptc ARG=d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,d13,d14,d15,d16,d17,d18,d19,d20,d21,d22,d23,d24,d25,d26,d27,d28,d29,d30,d31,d32,d33,d34,d35,d36,d37,d38,d39,d40,d41,d42,d43,d44,d45
+deep: PYTORCH_MODEL FILE=../deeptica/model.ptc ARG=d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,d13,d14,d15,d16,d17,d18,d19,d20,d21,d22,d23,d24,d25,d26,d27,d28,d29,d30,d31,d32,d33,d34,d35,d36,d37,d38,d39,d40,d41,d42,d43,d44,d45
 # bias 
 opes: OPES_METAD ARG=deep.node-0,deep.node-1 TEMP=300 PACE=500 FILE=KERNELS BARRIER=40 STATE_WFILE=RestartKernels STATE_WSTRIDE=500*10
  
@@ -64,4 +54,4 @@ ENDPLUMED
 
 #-- run gromacs --#
 execute("cp script/input.* script/plumed_descriptors.data script/run_gromacs.sh "+folder,folder=".")
-#execute("./run_gromacs.sh",folder=folder)
+execute("./run_gromacs.sh",folder=folder)
